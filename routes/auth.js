a8fa01ab2fb33f7accc6d5ac1c845cea623c8cee
prasanna <uiintern.infoxit@gmail.com>
2022-09-23 15:37:33 +0545

    checking stash

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router=express.Router()
 const authController=require('../controllers/authController')
 
-//authentications routes here
+//authentications register routes here
 
 router.post('/register',authController.saveUser)
 
