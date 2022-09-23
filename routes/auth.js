8fce76e7041af726821950d74a0dc6ae129b0d4a
prasanna <uiintern.infoxit@gmail.com>
2022-09-23 15:21:10 +0545

    added comment

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express=require('express')
 const router=express.Router()
 const authController=require('../controllers/authController')
 
+//auth routes here
+
 router.post('/register',authController.saveUser)
 
 router.post('/login',authController.login)
